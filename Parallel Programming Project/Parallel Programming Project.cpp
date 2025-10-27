7ff6923f300c87138bcb30d07cee296901806138
Sadra <sadra.afshar82@gmail.com>
2025-10-27 17:03:48 +0330

    Now with flags and vector

--- Parallel Programming Project/Parallel Programming Project.cpp
+++ Parallel Programming Project/Parallel Programming Project.cpp
@@ -2,48 +2,75 @@
 #include<time.h>
 #include <chrono>
 #include<vector>
+#include <fstream>
+#include <immintrin.h>
 
 #define Bound 10000
 
 int main()
 {
+
     srand(time(NULL));
     using namespace std;
+    ofstream file("Data with flag.txt");
+    /*
     int Am[Bound];
     int Bm[Bound];
-    for (int i = 0; i < Bound; i++)
+    file << "First test with array, time(in seconds) :\n";
+    for (int i = 0; i < 40; i++)
     {
-        Am[i] = rand()%100;
-        Bm[i] = rand()%100;
-    }
-    int Cm[Bound];
-    
-    //Classis Coding:
-    auto start = std::chrono::high_resolution_clock::now();
-    for (int i = 0; i < Bound; i++)
-    {
-        Cm[i] = Am[i] + Bm[i];
-    }
-    auto end = std::chrono::high_resolution_clock::now();
-    chrono::duration<double> result = end - start;
-    cout << "First test with array, time: " << result.count() << " seconds\n";
 
+
+        for (int i = 0; i < Bound; i++)
+        {
+            Am[i] = rand() % 100;
+            Bm[i] = rand() % 100;
+        }
+        int Cm[Bound];
+
+        //Classis Coding:
+        auto start = std::chrono::high_resolution_clock::now();
+        for (int i = 0; i < Bound; i++)
+        {
+            Cm[i] = Am[i] + Bm[i];
+        }
+        auto end = std::chrono::high_resolution_clock::now();
+        chrono::duration<double> result = end - start;
+        file << result.count() << "\n";
+    }
+    */
     // NOW vector:
-    vector<int> Av, Bv, Cv(Bound);
-    for (int i = 0; i < Bound; i++)
-    {
-        Av.push_back(rand() % 100);
-        Bv.push_back(rand() % 100);
-    }
-    auto start2 = chrono::high_resolution_clock::now();
-    for(int i = 0; i < Bound; i++)
-    {
-        Cv[i] = Av[i] + Bv[i];
-    }
-    auto end2 = chrono::high_resolution_clock::now();
-    chrono::duration<double> result2 = end2 - start2;
-    cout << "second test with Vector, No flag, time: " << result2.count() << " seconds\n";
+    alignas(32) int Av[Bound];
+    alignas(32) int Bv[Bound];
+    alignas(32) int Cv[Bound];
 
+    file << "third test with Vector and opt, time(in second): \n";
+    for (int test = 0; test < 40; ++test) {
+
+        for (int i = 0; i < Bound; ++i) {
+            Av[i] = rand() % 100;
+            Bv[i] = rand() % 100;
+        }
+
+        // SIMD Vector Addition using AVX2
+        auto start = std::chrono::high_resolution_clock::now();
+        int i = 0;
+        for (; i + 8 <= Bound; i += 8) {
+
+            __m256i va = _mm256_load_si256((__m256i*) & Av[i]);
+            __m256i vb = _mm256_load_si256((__m256i*) & Bv[i]);
+
+            __m256i vc = _mm256_add_epi32(va, vb);
+
+            _mm256_store_si256((__m256i*) & Cv[i], vc);
+        }
+        // Handle remaining
+        for (; i < Bound; ++i) {
+            Cv[i] = Av[i] + Bv[i];
+        }
+        auto end = std::chrono::high_resolution_clock::now();
+        std::chrono::duration<double> elapsed = end - start;
+        file << elapsed.count() << "\n";
+    }
     return 0;
 }
-
