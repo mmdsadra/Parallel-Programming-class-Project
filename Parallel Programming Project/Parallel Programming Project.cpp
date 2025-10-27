c82a91fece13995343abec1f0d412e11f699aef7
Sadra <sadra.afshar82@gmail.com>
2025-10-27 15:53:28 +0330

    adding problem!!

--- Parallel Programming Project/Parallel Programming Project.cpp
+++ Parallel Programming Project/Parallel Programming Project.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include<time.h>
 #include <chrono>
+#include<vector>
 
 #define Bound 10000
 
@@ -25,7 +26,19 @@ int main()
     }
     auto end = std::chrono::high_resolution_clock::now();
     chrono::duration<double> result = end - start;
-    cout << "Elapsed time: " << result.count() << " seconds\n";
+    cout << "First test with array, time: " << result.count() << " seconds\n";
+
+    // NOW vector:
+    vector<int> Av, Bv, Cv;
+    for (int i = 0; i < Bound; i++)
+    {
+        Av.push_back(rand() % 100);
+        Bv.push_back(rand() % 100);
+    }
+    auto start = std::chrono::high_resolution_clock::now();
+    Cv = Av + Bv;
+    
+    auto end = std::chrono::high_resolution_clock::now();
     return 0;
 }
 
