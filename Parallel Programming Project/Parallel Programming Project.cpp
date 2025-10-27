7f141c1983b0b03db4c71a1a22b91fe67e39c36b
Sadra <sadra.afshar82@gmail.com>
2025-10-27 15:48:26 +0330

    first edit. not paralllel

--- Parallel Programming Project/Parallel Programming Project.cpp
+++ Parallel Programming Project/Parallel Programming Project.cpp
@@ -1,9 +1,31 @@
-
-
 #include <iostream>
+#include<time.h>
+#include <chrono>
+
+#define Bound 10000
 
 int main()
 {
+    srand(time(NULL));
+    using namespace std;
+    int Am[Bound];
+    int Bm[Bound];
+    for (int i = 0; i < Bound; i++)
+    {
+        Am[i] = rand()%100;
+        Bm[i] = rand()%100;
+    }
+    int Cm[Bound];
     
+    //Classis Coding:
+    auto start = std::chrono::high_resolution_clock::now();
+    for (int i = 0; i < Bound; i++)
+    {
+        Cm[i] = Am[i] + Bm[i];
+    }
+    auto end = std::chrono::high_resolution_clock::now();
+    chrono::duration<double> result = end - start;
+    cout << "Elapsed time: " << result.count() << " seconds\n";
+    return 0;
 }
 
