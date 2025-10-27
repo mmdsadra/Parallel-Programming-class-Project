a70bb4a35642db569b2201ef82b6ee099a2135fa
Sadra <sadra.afshar82@gmail.com>
2025-10-27 16:08:12 +0330

    Okey now lets sign in a text and compare Ox more and disabled.

--- Parallel Programming Project/Parallel Programming Project.cpp
+++ Parallel Programming Project/Parallel Programming Project.cpp
@@ -29,16 +29,21 @@ int main()
     cout << "First test with array, time: " << result.count() << " seconds\n";
 
     // NOW vector:
-    vector<int> Av, Bv, Cv;
+    vector<int> Av, Bv, Cv(Bound);
     for (int i = 0; i < Bound; i++)
     {
         Av.push_back(rand() % 100);
         Bv.push_back(rand() % 100);
     }
-    auto start = std::chrono::high_resolution_clock::now();
-    Cv = Av + Bv;
-    
-    auto end = std::chrono::high_resolution_clock::now();
+    auto start2 = chrono::high_resolution_clock::now();
+    for(int i = 0; i < Bound; i++)
+    {
+        Cv[i] = Av[i] + Bv[i];
+    }
+    auto end2 = chrono::high_resolution_clock::now();
+    chrono::duration<double> result2 = end2 - start2;
+    cout << "second test with Vector, No flag, time: " << result2.count() << " seconds\n";
+
     return 0;
 }
 
